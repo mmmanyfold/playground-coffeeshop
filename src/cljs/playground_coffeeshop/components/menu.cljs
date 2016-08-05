25b258d11aab243ae9c2e609bb68c840ae69e7e8
eemshi <oreneemshi@gmail.com>
2016-08-05 02:18:06 -0600

    fix/clean up styles and refactor views into menu-component

--- src/cljs/playground_coffeeshop/components/menu.cljs
+++ src/cljs/playground_coffeeshop/components/menu.cljs
@@ -1,27 +1,49 @@
 (ns playground-coffeeshop.components.menu
-  (:require [re-frame.core :as re-frame]))
+  (:require [re-frame.core :as re-frame]
+            [playground-coffeeshop.views.about :refer [about-view]]
+            [playground-coffeeshop.views.events :refer [events-view]]
+            [playground-coffeeshop.views.home :refer [home-view]]
+            [playground-coffeeshop.views.bookings :refer [bookings-view]]
+            [playground-coffeeshop.views.contact :refer [contact-view]]
+            [playground-coffeeshop.components.header :refer [header-component]]))
+
+(defmulti views identity)
+(defmethod views :home-view [] [home-view])
+(defmethod views :about-view [] [about-view])
+(defmethod views :events-view [] [events-view])
+(defmethod views :bookings-view [] [bookings-view])
+(defmethod views :contact-view [] [contact-view])
+(defmethod views :default [] [:div "404"])
+
+(defn show-view
+  [view-name]
+  [views view-name])
 
 (defn menu-component []
-  (fn []
-    ;[:ul
-    ; [:li [:a {:href "/events"} [:div.menu-events]]]
-    ; [:li [:a {:on-click #(re-frame/dispatch [:display-upcoming-events])}
-    ;       [:span "upcoming"]]]
-    ; [:li [:a {:on-click #(re-frame/dispatch [:display-past-events])}
-    ;       [:span "past-events"]]]]
+  (let [active-view (re-frame/subscribe [:active-view])]
+    (fn []
+      ;[:ul
+      ; [:li [:a {:href "/events"} [:div.menu-events]]]
+      ; [:li [:a {:on-click #(re-frame/dispatch [:display-upcoming-events])}
+      ;       [:span "upcoming"]]]
+      ; [:li [:a {:on-click #(re-frame/dispatch [:display-past-events])}
+      ;       [:span "past-events"]]]]
 
 
-    ; .menu-item-divided.pure-menu-selected
+      ; .menu-item-divided.pure-menu-selected
 
-    [:div#layout.menu
-      [:a#menuLink.menu-link
-        {:href "#menu"}
-        [:span]]
-      [:div#menu
-        [:div.pure-menu
-         [:ul.pure-menu-list
-          [:li.pure-menu-item [:a.pure-menu-link {:href "/about"} "About"]]
-          [:li.pure-menu-item [:a.pure-menu-link {:href "/bookings"} "Bookings"]]
-          [:li.menu-item-divided.pure-menu-selected [:a.pure-menu-link {:href "/events"} "Events"]]
-          [:li.pure-menu-item [:a.pure-menu-link {:href "/merch"} "Merch"]]
-          [:li.pure-menu-item [:a.pure-menu-link {:href "/contact"} "Contact"]]]]]]))
+      [:div#layout
+        [:a#menuLink.menu-link
+          {:href "#menu"}
+          [:span]]
+        [:div#menu
+          [:div.pure-menu
+           [:ul.pure-menu-list
+            [:li.pure-menu-item [:a.pure-menu-link {:href "/about"} "About"]]
+            [:li.pure-menu-item [:a.pure-menu-link {:href "/bookings"} "Bookings"]]
+            [:li.pure-menu-item [:a.pure-menu-link {:href "/events"} "Events"]]
+            [:li.pure-menu-item [:a.pure-menu-link {:href "/merch"} "Merch"]]
+            [:li.pure-menu-item [:a.pure-menu-link {:href "/contact"} "Contact"]]]]]
+        [:div.main
+          [:div.content
+            [show-view @active-view]]]])))
