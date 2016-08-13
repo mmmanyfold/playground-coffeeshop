7a593dd965458b36df9ea30eb1ed0e2e26443638
dviramontes <dviramontes@gmail.com>
2016-08-13 11:35:27 -0600

    begins formatting dates

--- src/cljs/playground_coffeeshop/components/event-thumb.cljs
+++ src/cljs/playground_coffeeshop/components/event-thumb.cljs
@@ -1,18 +1,17 @@
-(ns playground-coffeeshop.components.event-thumb)
+(ns playground-coffeeshop.components.event-thumb
+  (:require cljsjs.moment))
 
 (defn event-thumb-component [data]
-  (let [title (get-in data [:title])
-        start (get-in data [:start])
-        end (get-in data [:end])
-        img-src (get-in data [:img-src])
-        cost (get-in data [:cost])
-        details (get-in data [:description])]
+  (let [{:keys [title start end img-src cost description]} data
+        formatted-start (.format (js/moment start) "LLLL")
+        formatted-end (.format (js/moment end) "LLLL")]
+
     [:div.event-thumb
      [:img {:src   img-src
             :width "100%"}]
      [:h3 title]
      [:div
-      start [:br]
-      end [:br]
+      formatted-start [:br]
+      formatted-end [:br]
       cost [:br]]
      [:hr]]))
