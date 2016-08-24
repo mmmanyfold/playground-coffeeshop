55cab219dfd819218a1895989383d6d55f738506
dviramontes <dviramontes@gmail.com>
2016-08-24 09:52:37 -0600

    adds counter balance, modifies catapult

--- src/cljs/playground_coffeeshop/components/header.cljs
+++ src/cljs/playground_coffeeshop/components/header.cljs
@@ -6,6 +6,8 @@
 (def Render (.-Render js/Matter))
 (def World (.-World js/Matter))
 (def Bodies (.-Bodies js/Matter))
+(def Composites (.-Composites js/Matter))
+(def Constraint (.-Constraint js/Matter))
 
 ; constants
 (defonce WIDTH 400)
@@ -13,24 +15,25 @@
 (defonce HW (/ WIDTH 2))
 (defonce HH (/ WIDTH 2))
 (defonce COLOR
-         {:render {:fillStyle   "black"
+         {:render {:fillStyle   "transparent"
                    :strokeStyle "black"
-                   :lineWidth   3}})
+                   :lineWidth   1}})
 
 ; create an engine
 (def engine (.create Engine))
 
 ; create object primitives
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;; x, y, w, h, opts
-(def boxA (.rectangle Bodies (- HW 75) 0 20 20))
-(def boxB (.rectangle Bodies (- HW 75) 0 20 20))
 
-(def projectile (.circle Bodies (- HW 75) 0 50))
+(def stack (.stack Composites 15 0 12 10 0 0
+                   (fn [x y]
+                     (.rectangle Bodies x y 10 10 (clj->js COLOR)))))
 
-(def catapult (.rectangle Bodies HW (- HH 100)
-                          200 10 (clj->js COLOR)))
+(def projectile (.circle Bodies (+ HW 75) 0 50 (clj->js COLOR)))
 
-(def triangle (.rectangle Bodies HW (- HH 25)
+(def catapult (.rectangle Bodies HW (- HH 45) 200 10 (clj->js COLOR)))
+
+(def counter-balance (.rectangle Bodies (- HW 100) (- HH 30)
                           20 20 (clj->js (assoc COLOR
                                            :isStatic true))))
 
@@ -43,14 +46,19 @@
                              render
                              (.create Render #js {:element (.querySelector js/document "#canvas")
                                                   :engine  engine
-                                                  :options #js {:wireframes          false
-                                                                :background          "transparent"
-                                                                :width               WIDTH
-                                                                :height              HEIGHT}})]
+                                                  :options #js {:wireframes false
+                                                                :background "transparent"
+                                                                :width      WIDTH
+                                                                :height     HEIGHT}})]
 
                          ; add all of the bodies to the world
                          (.add World (.-world engine)
-                               #js [boxA boxB projectile catapult triangle])
+                               #js [stack
+                                    catapult
+                                    counter-balance
+                                    projectile
+                                    (.create Constraint (clj->js {:bodyA catapult :pointB {:x (- HW 20) :y (+ HH 10)}}))
+                                    (.create Constraint (clj->js {:bodyA catapult :pointB {:x (+ HW 20) :y (+ HH 10)}}))])
 
                          ; run the engine
                          (.run Engine engine)
@@ -62,10 +70,11 @@
        :reagent-render
                      (fn [data]
                        [:div.header.flex-row
-                        [:div.left
+                        [:div.left.flex-col
                          [:a {:href "/"}
-                          [:img.logo {:src "img/header_logo.png"}]]]
-                        [:div.right.flex-col
+                          [:img.logo {:src "img/header_logo.png"
+                                      :width "80%"}]]]
+                        [:div.right
                          [:div
                           [:img.addy {:src "img/header_addy.png"}]]
                          [:div#canvas]]
