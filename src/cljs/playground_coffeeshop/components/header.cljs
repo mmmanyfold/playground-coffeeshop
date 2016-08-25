a80711e0a0b8935f870743bd07de06d1aa9a2de1
dviramontes <dviramontes@gmail.com>
2016-08-25 00:24:36 -0600

    randomize the returned primitive in stack function, adds color white

--- src/cljs/playground_coffeeshop/components/header.cljs
+++ src/cljs/playground_coffeeshop/components/header.cljs
@@ -14,7 +14,11 @@
 (defonce HEIGHT 200)
 (defonce HW (/ WIDTH 2))
 (defonce HH (/ WIDTH 2))
-(defonce COLOR
+(defonce BLACK-COLOR
+         {:render {:fillStyle   "black"
+                   :strokeStyle "black"
+                   :lineWidth   1}})
+(defonce WHITE-COLOR
          {:render {:fillStyle   "transparent"
                    :strokeStyle "black"
                    :lineWidth   1}})
@@ -25,17 +29,27 @@
 ; create object primitives
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;; x, y, w, h, opts
 
-(def stack (.stack Composites 15 0 12 10 0 0
-                   (fn [x y]
-                     (.rectangle Bodies x y 10 10 (clj->js COLOR)))))
+(def stack (.. Composites
+               (stack 275 0 1 10 1 0
+                      (fn [x y]
+                        (let [r (rand-int 5)
+                              bc (clj->js BLACK-COLOR)
+                              wc (clj->js WHITE-COLOR)]
+                          (case (mod r 5)
+                            0 (.rectangle Bodies x y 10 10 bc)
+                            1 (.circle Bodies x y 8 bc)
+                            2 (.polygon Bodies x y 5 8 wc)
+                            3 (.circle Bodies x y 8 wc)
+                            4 (.polygon Bodies x y 3 8 wc)  ;; triangles
+                            5 (.polygon Bodies x y 3 8 bc)))))))
 
-(def projectile (.circle Bodies (+ HW 75) 0 50 (clj->js COLOR)))
+(def weight (.circle Bodies (- HW 100) 0 50 (clj->js (assoc BLACK-COLOR :density 0.3))))
 
-(def catapult (.rectangle Bodies HW (- HH 45) 200 10 (clj->js COLOR)))
+(def catapult (.rectangle Bodies HW (- HH 45) 200 10 (clj->js BLACK-COLOR)))
 
-(def counter-balance (.rectangle Bodies (- HW 100) (- HH 30)
-                          20 20 (clj->js (assoc COLOR
-                                           :isStatic true))))
+(def counter-balance (.rectangle Bodies (+ HW 75) (- HH 30) 20 20
+                                 (clj->js (assoc BLACK-COLOR
+                                            :isStatic true))))
 
 (defn header-component []
   (let []
@@ -56,7 +70,7 @@
                                #js [stack
                                     catapult
                                     counter-balance
-                                    projectile
+                                    weight
                                     (.create Constraint (clj->js {:bodyA catapult :pointB {:x (- HW 20) :y (+ HH 10)}}))
                                     (.create Constraint (clj->js {:bodyA catapult :pointB {:x (+ HW 20) :y (+ HH 10)}}))])
 
@@ -72,7 +86,7 @@
                        [:div.header.flex-row
                         [:div.left.flex-col
                          [:a {:href "/"}
-                          [:img.logo {:src "img/header_logo.png"
+                          [:img.logo {:src   "img/header_logo.png"
                                       :width "80%"}]]]
                         [:div.right
                          [:div
