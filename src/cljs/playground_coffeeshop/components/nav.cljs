a657ac5350af2d23de326007cd1ce7106be4402d
eemshi <oreneemshi@gmail.com>
2017-02-01 22:49:07 -0700

    refactors main-view and home-view, adds responsive youtube embed, moves carousel to about.cljs

--- src/cljs/playground_coffeeshop/components/nav.cljs
+++ src/cljs/playground_coffeeshop/components/nav.cljs
@@ -1,63 +1,32 @@
 (ns playground-coffeeshop.components.nav
-  (:require [re-frame.core :as re-frame]
-            [playground-coffeeshop.views.about :refer [about-view]]
-            [playground-coffeeshop.views.news :refer [news-view]]
-            [playground-coffeeshop.views.events :refer [events-view]]
-            [playground-coffeeshop.views.home :refer [home-view]]
-            [playground-coffeeshop.views.bookings :refer [bookings-view]]
-            [playground-coffeeshop.views.contact :refer [contact-view]]
-            [playground-coffeeshop.components.header :refer [header-component]]
-            [playground-coffeeshop.views.menus :refer [menus-view]]
-            [playground-coffeeshop.views.consignment :refer [consignment-view]]
-            [playground-coffeeshop.views.event :refer [event-view]]))
-
-;; TODO: move this back to views.cljs
-
-(defmulti views identity)
-(defmethod views :home-view [] [home-view])
-(defmethod views :about-view [] [about-view])
-(defmethod views :news-view [] [news-view])
-(defmethod views :events-view [] [events-view])
-(defmethod views :bookings-view [] [bookings-view])
-(defmethod views :contact-view [] [contact-view])
-(defmethod views :event-view [] [event-view])
-(defmethod views :menus-view [] [menus-view])
-(defmethod views :consignment-view [] [consignment-view])
-(defmethod views :default [] [:div "404"])
-
-(defn show-view
-  [view-name]
-  [views view-name])
+  (:require [re-frame.core :as re-frame]))
 
 (defn nav-component []
   (let [active-view (re-frame/subscribe [:active-view])]
     (fn []
-      [:div.flex-row
-       [:div#nav-wrapper
-         [:ul.nav
-          [:li [:a {:href "/about"} "About"]]
-          ; [:li [:a {:href "/menus"} "Menus"]]
-          [:li [:a {:href "/news"} "News"]]
-          [:ul.events-menu [:span [:a
-                                   {:href     "/events"
-                                    :on-click #(re-frame/dispatch [:display-filtered-events])} "Events"]]
-           [:li
-            {:class (if (= @active-view :events-view)
-                      "events-submenu-show"
-                      "events-submenu-hide")}
-            [:a {:href     "/events"
-                 :on-click #(re-frame/dispatch [:display-filtered-events >])}
-             [:span "Upcoming"]]]
-           [:li.last
-            {:class (if (= @active-view :events-view)
-                      "events-submenu-show"
-                      "events-submenu-hide")}
-            [:a {:href     "/events"
-                 :on-click #(re-frame/dispatch [:display-filtered-events <])}
-             [:span "Past"]]]]
-          [:li [:a {:href "/bookings"} "Bookings"]]
-          [:li [:a {:href "/consignment"} "Consignment"]]
-          [:li [:a {:href "/contact"} "Contact"]]]]
-       [:div.main
-        [:div.content
-         [show-view @active-view]]]])))
+     [:div#nav-wrapper {:style (when (= @active-view :home-view)
+                                 {:position "absolute"})}
+       [:ul.nav
+        [:li [:a {:href "/about"} "About"]]
+        ; [:li [:a {:href "/menus"} "Menus"]]
+        [:li [:a {:href "/news"} "News"]]
+        [:ul.events-menu [:span [:a
+                                 {:href     "/events"
+                                  :on-click #(re-frame/dispatch [:display-filtered-events])} "Events"]]
+         [:li
+          {:class (if (= @active-view :events-view)
+                    "events-submenu-show"
+                    "events-submenu-hide")}
+          [:a {:href     "/events"
+               :on-click #(re-frame/dispatch [:display-filtered-events >])}
+           [:span "Upcoming"]]]
+         [:li.last
+          {:class (if (= @active-view :events-view)
+                    "events-submenu-show"
+                    "events-submenu-hide")}
+          [:a {:href     "/events"
+               :on-click #(re-frame/dispatch [:display-filtered-events <])}
+           [:span "Past"]]]]
+        [:li [:a {:href "/bookings"} "Bookings"]]
+        [:li [:a {:href "/consignment"} "Consignment"]]
+        [:li [:a {:href "/contact"} "Contact"]]]])))
--- src/cljs/playground_coffeeshop/views/about.cljs
+++ src/cljs/playground_coffeeshop/views/about.cljs
@@ -1,24 +1,29 @@
 (ns playground-coffeeshop.views.about
   (:require [re-frame.core :as re-frame]
-            [reagent.core :as reagent]))
-
-(defn- generic-responsive-iframe
-  "returns an responsive iframe"
-  [iframe-code]
-  (.replace iframe-code (js/RegExp. "/\\\"/g,'\\''")))
+            [reagent.core :as reagent]
+            [cljsjs.jquery :as $]))
 
 (defn about-view []
-  (let [about (re-frame/subscribe [:on-about-entry-render])]
+  (let [about (re-frame/subscribe [:on-about-entry-render])
+        slide-show-images (re-frame/subscribe [:on-slide-show-images])]
     (reagent/create-class
       {:component-will-mount
        (fn []
          (when (empty? @about)
            (re-frame/dispatch [:get-site-cms-data])))
+       :component-did-mount
+       #(.carousel (js/$ ".carousel"))
        :reagent-render
        (fn []
-         (let [{:keys [title description video]} (:fields @about)]
+         (let [{:keys [description]} (:fields @about)]
            [:div
-            [:div.embed-container
-             {"dangerouslySetInnerHTML"
-              #js{:__html (generic-responsive-iframe video)}}]
+            [:div.carousel.slide {:data-ride "carousel"}
+             [:div.carousel-inner
+              (let [first-image (first @slide-show-images)]
+                (for [src @slide-show-images
+                      :let [id (gensym "key-")
+                            is-first? (= first-image src)]]
+                  ^{:key id}
+                  [:div.item {:class (when is-first? "active")}
+                   [:img {:src src, :alt "Playground Coffee Shop"}]]))]]
             [:p description]]))})))
