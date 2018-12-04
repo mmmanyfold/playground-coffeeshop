594e7cac8b40e5e036cd7f33732cef49da593737
eemshi <oreneemshi@gmail.com>
2018-12-04 16:11:51 -0700

    use youtube video id from contentful

--- src/cljs/playground_coffeeshop/views.cljs
+++ src/cljs/playground_coffeeshop/views.cljs
@@ -1,5 +1,6 @@
 (ns playground-coffeeshop.views
   (:require [re-frame.core :as re-frame]
+            [reagent.core :as reagent]
             [playground-coffeeshop.components.header :refer [header-component]]
             [playground-coffeeshop.components.nav :refer [nav-component]]
             [playground-coffeeshop.components.footer :refer [footer-component]]
@@ -30,30 +31,38 @@
 
 
 (defn main-view []
- (let [active-view (re-frame/subscribe [:active-view])]
-  (fn []
-    [:div
-     [header-component]
-     (if (= @active-view :home-view)
-       [:div
-        [:div.flex-row
-          [nav-component]
-          [:div.main
-           [:div.embed-container
-            [:iframe {:src "https://www.youtube.com/embed/maRzI4b_CbA?rel=0&autoplay=1&controls=0&showinfo=0"
-                      :frameBorder 0
-                      :allowFullScreen true}]]
-           [:div.flex-row-wrap.players
-            [:div [:a {:href "https://soundcloud.com/playground_youth"} "Soundcloud"]]
-            [:div "*"]
-            [:div [:a {:href "http://mixlr.com/playgroundradio/" } "Mixlr Radio"]]
-            [:iframe {:width "48%", :scrolling "no", :frameborder "no", :src "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/541285623&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"}]
-            [:iframe {:src "https://mixlr.com/playgroundradio/embed", :width "48%", :scrolling "no", :frameborder "no", :marginheight "0", :marginwidth "0"}]]]]
-        [footer-component]]
-       [:div
-        [:div.flex-row
-         [nav-component]
-         [:div.main
-          [:div.content
-           [show-view @active-view]]]]
-        [footer-component]])])))
+  (let [active-view (re-frame/subscribe [:active-view])
+        video (re-frame/subscribe [:on-consignment-entry-render])]
+    (reagent/create-class
+       {:component-will-mount
+        (fn []
+         (when (empty? @video)
+           (re-frame/dispatch [:get-site-cms-data])))
+        :reagent-render
+        (fn []
+          (let [{:keys [youtubeId]} (:fields @video)]
+            [:div
+             [header-component]
+             (if (= @active-view :home-view)
+               [:div
+                [:div.flex-row
+                  [nav-component]
+                  [:div.main
+                   [:div.embed-container
+                    [:iframe {:src (str "https://www.youtube.com/embed/" youtubeId "?rel=0&autoplay=1&controls=0&showinfo=0")
+                              :frameBorder 0
+                              :allowFullScreen true}]]
+                   [:div.flex-row-wrap.players
+                    [:div [:a {:href "https://soundcloud.com/playground_youth"} "Soundcloud"]]
+                    [:div "*"]
+                    [:div [:a {:href "http://mixlr.com/playgroundradio/" } "Mixlr Radio"]]
+                    [:iframe {:width "48%", :scrolling "no", :frameBorder "no", :src "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/541285623&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"}]
+                    [:iframe {:src "https://mixlr.com/playgroundradio/embed", :width "48%", :scrolling "no", :frameBorder "no", :marginHeight "0", :marginWidth "0"}]]]]
+                [footer-component]]
+               [:div
+                [:div.flex-row
+                 [nav-component]
+                 [:div.main
+                  [:div.content
+                   [show-view @active-view]]]]
+                [footer-component]])]))})))
